456b6dd7f7b4206506e19cd4faaa84537381885f
CS Adnan <adnan.pri@gmail.com>
2022-05-08 03:34:25 +0600

    add verbose install.sh output

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #/bin/sh
 
+echo "Installing dependencies"
 npm install
+
+echo "Building app"
 npm run package
+
+echo "Creating folders in ~/.config and /opt"
 mkdir -p ~/.config/NerdChat/favicon
 sudo mkdir -p /opt/NerdChat
+
+echo "Copying files"
 sudo cp -r dist/NerdChat-linux-x64/* /opt/NerdChat
+
+echo "Creating desktop entry"
 sudo cp nerdchat.desktop /usr/share/applications
+
+echo "Done."
